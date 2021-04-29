574244a683b5f2fee7d7f5f3a99d296cbc94a221
Avikal Darolia <avikaldarolia7@gmail.com>
2021-04-29 23:09:14 +0530

    another hook

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,9 @@
-
-
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
 
 export default function Timeline() {
+  // we need to get the login photos
+  const { photos } = usePhotos();
   return (
     <div className='container col-span-2'>
       <p>I am the timeline</p>
