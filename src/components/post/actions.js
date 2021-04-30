a60a66d48dbea609374cc900ae9b0008c2047e0c
Avikal Darolia <avikaldarolia7@gmail.com>
2021-04-30 20:37:33 +0530

    comments

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -85,7 +85,7 @@ export default function Actions({
           </svg>
         </div>
       </div>
-      <div className='f-4 py-0'>
+      <div className='p-4 py-0'>
         <p className='font-bold'>
           {likes === 1 ? `${likes} like` : `${likes} likes`}
         </p>
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 import Header from "./header";
 import Image from "./image";
 import Actions from "./actions";
+import Footer from "./footer";
+import Comments from "./comments";
+
 export default function Post({ content }) {
   const commentInput = useRef(null);
-    console.log("userLikedPhoto",content.userLikedPhoto)
+  console.log("userLikedPhoto", content.userLikedPhoto);
   const handleFocus = () => commentInput.current.focus();
   // components
   // => header, image, actions (like and comment icons), footer, comments
   //   console.log("content", content);
   return (
-
     <div className='rounded col-span-4 border bg-white border-gray-primary mb-12'>
       <Header username={content.username} />
       <Image src={content.imageSrc} caption={content.caption} />
@@ -20,6 +22,12 @@ export default function Post({ content }) {
         totalLikes={content.likes.length}
         userLikedPhoto={content.userLikedPhoto}
         handleFocus={handleFocus}
+      />
+      <Footer username={content.username} caption={content.caption} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        commentInput={commentInput}
       />
     </div>
   );
