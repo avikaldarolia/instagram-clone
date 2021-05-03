511b87d3565eef5d063a7645ba3fe7ce4e5e8d0d
Avikal Darolia <avikaldarolia7@gmail.com>
2021-05-04 01:29:47 +0530

    Fixed Not Found

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
-
+import Header from "../components/header";
 export default function NotFound() {
   useEffect(() => {
     document.title = `Not Found!`;
   }, []);
   return (
     <div className='bg-gray-background'>
+      <Header />
       <div className='mx-auth max-w-screen-lg'>
         <p className='text-center text-2xl'>Not Found!</p>
       </div>
